97eadf5456cbe5cee17ec1919a719e933cc751c6
mnguywn <90582041+mnguywn@users.noreply.github.com>
2021-10-18 11:44:06 -0400

    Update amicable.js

--- amicable.js
+++ amicable.js
@@ -42,7 +42,7 @@ function checkIfAmicable() {
     var num1 = readNum1();
     var num2 = readNum2();
     var num1Sum = addFactors(getFactors(num1));
-    alert("The factors for are for " + num1 + " are " + getFactors(num1));
+    alert("The factors for " + num1 + " are " + getFactors(num1));
     alert("The factors for " + num2 + " are " + getFactors(num2));
 
     var num2Sum = addFactors(getFactors(num2));
